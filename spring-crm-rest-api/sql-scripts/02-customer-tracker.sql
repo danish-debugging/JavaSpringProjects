6d55f5c2fae88473927c2f374e08541ea56aae2c
danish-debugging <66498948+danish-debugging@users.noreply.github.com>
2023-06-28 19:52:06 -0400

    Add files via upload
    Version 1.0

---
+++ spring-crm-rest-api/sql-scripts/02-customer-tracker.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE  IF NOT EXISTS `web_customer_tracker` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `web_customer_tracker`;
+-- MySQL dump 10.13  Distrib 5.6.13, for osx10.6 (i386)
+--
+-- Host: 127.0.0.1    Database: web_customer_tracker
+-- ------------------------------------------------------
+-- Server version	5.6.16
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer`
+--
+
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+
+INSERT INTO `customer` VALUES 
+	(1,'David','Adams','david@luv2code.com'),
+	(2,'John','Doe','john@luv2code.com'),
+	(3,'Ajay','Rao','ajay@luv2code.com'),
+	(4,'Mary','Public','mary@luv2code.com'),
+	(5,'Maxwell','Dixon','max@luv2code.com');
+
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+TION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
